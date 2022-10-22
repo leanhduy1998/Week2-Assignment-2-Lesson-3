644ebe02896af70af07bdc9844a9d4212e8259ce
Anh Duy Le <leanhduy1998@gmail.com>
2022-10-22 08:34:00 -0500

    Finished

--- app/src/main/java/com/example/week2_assignment_2_lesson_3/MainActivity.kt
+++ app/src/main/java/com/example/week2_assignment_2_lesson_3/MainActivity.kt
@@ -1,36 +1,52 @@
 package com.example.week2_assignment_2_lesson_3
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
 import com.example.week2_assignment_2_lesson_3.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    private var foods = ArrayList<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         setSupportActionBar(binding.toolbar)
 
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
 
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        foods.addAll( arrayOf<String>(
+            "Hamburger", "Pizza",
+            "Mexican", "American", "Chinese"
+        ))
+        val decideButton = findViewById<Button>(R.id.decideButtonId)
+        decideButton.setOnClickListener {
+            foods.shuffle()
+            val foodTextView = findViewById<TextView>(R.id.foodTextView)
+            foodTextView.text = foods.get(0)
+        }
+
+        val addFoodButton = findViewById<Button>(R.id.addFoodButtonId)
+        addFoodButton.setOnClickListener {
+            val addFoodTextView = findViewById<TextView>(R.id.addFoodTextView)
+            if (!addFoodTextView.text.isEmpty()) {
+                foods.add(addFoodTextView.text.toString())
+                addFoodTextView.text = ""
+            }
         }
     }
 
